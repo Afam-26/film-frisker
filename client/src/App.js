b38ddd7841a697eec9cd9daaaee5083624928ce2
Nehreet Kaur <kaur.nehreet@gmail.com>
2021-03-29 20:32:50 -0500

    pushing source folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import { Container } from "@material-ui/core"
 // import {BrowserRouter} from "react-router-dom"
 import './App.css';
--- client/src/Pages/Movies/Movies.js
+++ client/src/Pages/Movies/Movies.js
@@ -1,4 +1,4 @@
-
+import React from "react";
 import axios from "axios";
 import {useEffect, useState} from "react";
 import  MovieContentPage from "../../components/MovieContentPage/MovieContentPage"
